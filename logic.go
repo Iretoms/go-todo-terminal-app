d222cc014ffd3d74169e994acafd87a3efe8aa2b
Iretoms <iretomiwajonathan@gmail.com>
2024-06-11 18:23:35 +0100

    add, update, display and delete functionality

---
+++ logic.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var tasks []singleTask
+
+type singleTask struct {
+	description string
+	completed   bool
+}
+
+func userInput(r *bufio.Reader) (string, error) {
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
+
+func newTask(d string) singleTask {
+	t := singleTask{
+		description: d,
+		completed:   false,
+	}
+
+	return t
+}
+
+func convToInt(n string) int {
+	i, err := strconv.Atoi(n)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return i - 1
+}
+
+func getItemByIndex(i int) *singleTask {
+	return &tasks[i]
+}
+
+func addTask(t *singleTask) {
+	tasks = append(tasks, (*t))
+}
+
+func displayTask() {
+	if len(tasks) == 0 {
+		fmt.Println("No task created yet!")
+		todoActions()
+		return
+	}
+	for idx, task := range tasks {
+		desc := task.description
+		number := idx + 1
+		fmt.Printf("%v. %v", number, desc)
+		if task.completed == true {
+			fmt.Print(" -Completed\n")
+			return
+		}
+		fmt.Print(" -Not completed\n")
+	}
+}
+
+func updateTask() {
+	if len(tasks) == 0 {
+		fmt.Println("No task to update, add task!")
+		todoActions()
+		return
+	}
+
+	fmt.Print("Type the task number you want to update: ")
+	reader := bufio.NewReader(os.Stdin)
+	number, _ := userInput(reader)
+
+	conIdx := convToInt(number)
+
+	if conIdx < 0 || conIdx >= len(tasks) {
+		fmt.Println("Invalid task number.")
+		return
+	}
+
+	val := getItemByIndex(conIdx)
+
+	fmt.Print("Type your updated task: ")
+	updated, _ := userInput(reader)
+
+	val.description = updated
+
+	fmt.Println("Task updated successfully!")
+	displayTask()
+}
+
+func remove(s []singleTask, i int) {
+	tasks = append(s[:i], s[i+1:]...)
+}
+
+func deleteTodo() {
+	if len(tasks) == 0 {
+		fmt.Println("No task to delete, add task!")
+		todoActions()
+		return
+	}
+
+	fmt.Print("Type the task number you want to delete: ")
+	reader := bufio.NewReader(os.Stdin)
+	num, _ := userInput(reader)
+	conIdx := convToInt(num)
+
+	if conIdx < 0 || conIdx >= len(tasks) {
+		fmt.Println("Invalid task number.")
+		return
+	}
+
+	remove(tasks, conIdx)
+	fmt.Println("Task deleted successfully!")
+	displayTask()
+}
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func todoActions() {
+	fmt.Println("Choose an option below")
+	fmt.Println("type (a to Add Todo, s to show All Todos, u to update a task, d to delete a task, e to Exit): ")
+	reader := bufio.NewReader(os.Stdin)
+	option, _ := userInput(reader)
+
+	switch option {
+	case "a":
+		fmt.Print("Type your todo: ")
+		taskDesc, _ := userInput(reader)
+		newT := newTask(taskDesc)
+		addTask(&newT)
+		fmt.Println("Todo added successfully!")
+		todoActions()
+	case "s":
+		displayTask()
+		todoActions()
+	case "u":
+		updateTask()
+		todoActions()
+	case "d":
+		deleteTodo()
+		todoActions()
+	case "e":
+		fmt.Println("Exited successfully! Have a nice day")
+	default:
+		fmt.Println("Not a valid option")
+		todoActions()
+	}
+
+}
+
+
+func main() {
+	fmt.Println("Welcome to your Todo List App")
+	todoActions()
+}
